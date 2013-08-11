fa5a93a20107312c160bc843765960dcad313233
Siôn Le Roux <sinisterstuf@gmail.com>
2013-08-11 16:26:12 +0200

    Add functionality to deal with pressing Yes/No in a guess

--- src/org/sinisterstuf/guesstheanimal/Guess.java
+++ src/org/sinisterstuf/guesstheanimal/Guess.java
@@ -3,9 +3,12 @@ package org.sinisterstuf.guesstheanimal;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class Guess extends Activity {
+	
+	private Animal animal;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -13,11 +16,38 @@ public class Guess extends Activity {
 		setContentView(R.layout.guess);
 		
 		Intent intent = getIntent();
-		Animal animal = (Animal) intent.getSerializableExtra("org.sinisterstuf.guesstheanimal.Animal");
+		animal = (Animal) intent.getSerializableExtra("org.sinisterstuf.guesstheanimal.Animal");
 		
 		// write guess text
 		TextView guessText = (TextView)findViewById(R.id.guessText);
 		guessText.setText(animal.question);
+	}
+	
+	/**
+	 * Called when the user chooses Yes
+	 */
+	public void parseYes(View view) {
+		parseChoice(true);
+	}
+
+	/**
+	 * Called when the user chooses No
+	 */
+	public void parseNo(View view) {
+		parseChoice(false);
+	}
+
+	/**
+	 * Compares the user's <code>choice</code> to the desired answer for this
+	 * <code>Animal</code> from <code>Animal.answerWhenMe</code>
+	 * @param choice the user's choice
+	 */
+	private void parseChoice(boolean choice) {
+		if (choice == animal.answerWhenMe) {
+			// they chose me!
+		} else {
+			// see if there's another animal...
+		}
 	}
 
 }
