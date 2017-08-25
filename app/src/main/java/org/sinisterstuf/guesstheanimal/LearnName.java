f9a80f44a3d986001cad80929dfb322440c1990a
ezisazis <levente.jonas@gmail.com>
2017-08-26 00:06:21 +0200

    Using butterknife on the LearnName screen

--- app/src/main/java/org/sinisterstuf/guesstheanimal/LearnName.java
+++ app/src/main/java/org/sinisterstuf/guesstheanimal/LearnName.java
@@ -3,31 +3,35 @@ package org.sinisterstuf.guesstheanimal;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.EditText;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class LearnName extends Activity {
-	
-	private Animal previous;
-	private boolean prevReq;
+
+    private Animal previous;
+    private boolean prevReq;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.learn_name);
+        ButterKnife.bind(this);
         Intent intent = getIntent();
-        previous = (Animal)intent.getSerializableExtra(Animal.ANIMAL);
+        previous = (Animal) intent.getSerializableExtra(Animal.ANIMAL);
         prevReq = intent.getBooleanExtra(Animal.NEXT_REQ, false);
     }
-    
-    public void learn(View view) {
-    	EditText textInput = (EditText)findViewById(R.id.userInput);
-    	String name = textInput.getText().toString();
-    	Intent intent = new Intent(this, LearnQuestion.class);
-    	intent.putExtra(Animal.NAME, name);
-    	intent.putExtra(Animal.ANIMAL, previous);
-    	intent.putExtra(Animal.NEXT_REQ, prevReq);
-    	startActivity(intent);
+
+    @OnClick(R.id.okButton)
+    public void learn() {
+        EditText textInput = (EditText) findViewById(R.id.userInput);
+        String name = textInput.getText().toString();
+        Intent intent = new Intent(this, LearnQuestion.class);
+        intent.putExtra(Animal.NAME, name);
+        intent.putExtra(Animal.ANIMAL, previous);
+        intent.putExtra(Animal.NEXT_REQ, prevReq);
+        startActivity(intent);
     }
-    
+
 }
