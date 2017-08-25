5e5ead8d7f44b4fba132e145ba2eefd52947402d
ezisazis <levente.jonas@gmail.com>
2017-08-26 00:00:14 +0200

    Using butterkinfe on the greeting screen

--- app/src/main/java/org/sinisterstuf/guesstheanimal/Greeting.java
+++ app/src/main/java/org/sinisterstuf/guesstheanimal/Greeting.java
@@ -3,27 +3,31 @@ package org.sinisterstuf.guesstheanimal;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class Greeting extends Activity {
-	
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.greeting);
+        ButterKnife.bind(this);
     }
 
-	public void guessAnAnimal(View view) {
-		Animal firstAnimal = new Animal("a pig", "Does it have a curly tail?",  true);
-		firstAnimal.prevAnimal = null;
-		firstAnimal.noAnimal = new Animal("a dog", "Does it have 4 legs?", true);
-		// reverse-linked list traversal:
-		firstAnimal.noAnimal.prevAnimal = firstAnimal;
-		Game.firstAnimal = firstAnimal;
-		Intent intent = new Intent(this, Guess.class);
-		intent.putExtra(Animal.ANIMAL, firstAnimal);
-		intent.putExtra(Guess.FINAL_GUESS, false);
-		startActivity(intent);
-	}
+    @OnClick(R.id.okButton)
+    public void guessAnAnimal() {
+        Animal firstAnimal = new Animal("a pig", "Does it have a curly tail?", true);
+        firstAnimal.prevAnimal = null;
+        firstAnimal.noAnimal = new Animal("a dog", "Does it have 4 legs?", true);
+        // reverse-linked list traversal:
+        firstAnimal.noAnimal.prevAnimal = firstAnimal;
+        Game.firstAnimal = firstAnimal;
+        Intent intent = new Intent(this, Guess.class);
+        intent.putExtra(Animal.ANIMAL, firstAnimal);
+        intent.putExtra(Guess.FINAL_GUESS, false);
+        startActivity(intent);
+    }
 
 }
