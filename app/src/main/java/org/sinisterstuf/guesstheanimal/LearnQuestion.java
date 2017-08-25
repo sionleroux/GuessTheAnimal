c695f7ad449afbb7891cc70964a998b787463c8a
ezisazis <levente.jonas@gmail.com>
2017-08-26 00:07:47 +0200

    Using butterknife on the LearnQuestion screen

--- app/src/main/java/org/sinisterstuf/guesstheanimal/LearnQuestion.java
+++ app/src/main/java/org/sinisterstuf/guesstheanimal/LearnQuestion.java
@@ -3,42 +3,50 @@ package org.sinisterstuf.guesstheanimal;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class LearnQuestion extends Activity {
-	
-	private Animal previous;
-	private boolean prevReq;
-	private String name;
+
+    @BindView(R.id.learnText)
+    TextView learnText;
+    @BindView(R.id.userInput)
+    EditText userInput;
+
+    private Animal previous;
+    private boolean prevReq;
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.learn_question);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
-        previous = (Animal)intent.getSerializableExtra(Animal.ANIMAL);
+        previous = (Animal) intent.getSerializableExtra(Animal.ANIMAL);
         prevReq = intent.getBooleanExtra(Animal.NEXT_REQ, false);
         name = intent.getStringExtra(Animal.NAME);
-        
+
         // set the text
-        TextView learnText = (TextView)findViewById(R.id.learnText);
         String templateText = learnText.getText().toString();
         String fullText = String.format(templateText, name, previous.name);
         learnText.setText(fullText);
     }
-    
-    public void learn(View view) {
-    	EditText textInput = (EditText)findViewById(R.id.userInput);
-    	String question = textInput.getText().toString();
-    	Intent intent = new Intent(this, LearnYesNo.class);
-    	intent.putExtra(Animal.ANIMAL, previous);
-    	intent.putExtra(Animal.NEXT_REQ, prevReq);
-    	intent.putExtra(Animal.NAME, name);
-    	intent.putExtra(Animal.QUESTION, question);
-    	startActivity(intent);
+
+    @OnClick(R.id.okButton)
+    public void learn() {
+        String question = userInput.getText().toString();
+        Intent intent = new Intent(this, LearnYesNo.class);
+        intent.putExtra(Animal.ANIMAL, previous);
+        intent.putExtra(Animal.NEXT_REQ, prevReq);
+        intent.putExtra(Animal.NAME, name);
+        intent.putExtra(Animal.QUESTION, question);
+        startActivity(intent);
     }
 
 }
