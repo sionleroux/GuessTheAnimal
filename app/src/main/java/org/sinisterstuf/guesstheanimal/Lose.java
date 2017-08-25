bbf05f810432b2704f4c8dc8ed3e3a8dd1019ef2
ezisazis <levente.jonas@gmail.com>
2017-08-26 00:11:07 +0200

    Using butterknife on the Loose screen

--- app/src/main/java/org/sinisterstuf/guesstheanimal/Lose.java
+++ app/src/main/java/org/sinisterstuf/guesstheanimal/Lose.java
@@ -3,21 +3,25 @@ package org.sinisterstuf.guesstheanimal;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class Lose extends Activity {
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.lose);
-	}
-	
-	public void startNewGame(View view) {
-		Intent intent = new Intent(this, Guess.class);
-		intent.putExtra(Animal.ANIMAL, Game.firstAnimal);
-		intent.putExtra(Guess.FINAL_GUESS, false);
-		startActivity(intent);
-	}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lose);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.okButton)
+    public void startNewGame() {
+        Intent intent = new Intent(this, Guess.class);
+        intent.putExtra(Animal.ANIMAL, Game.firstAnimal);
+        intent.putExtra(Guess.FINAL_GUESS, false);
+        startActivity(intent);
+    }
 
 }
