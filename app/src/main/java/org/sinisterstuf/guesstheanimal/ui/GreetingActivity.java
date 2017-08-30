3cb870f30c21a38ea11fa8202b309b1f063e19c9
ezisazis <levente.jonas@gmail.com>
2017-08-30 21:55:30 +0200

    Use Butterknife library
    Butterknife gives you the possibility to annotate the UI fields, so you
    don't have to mix logic inside xmls and java classes.
    
    Further information: http://jakewharton.github.io/butterknife/

--- app/src/main/java/org/sinisterstuf/guesstheanimal/ui/GreetingActivity.java
+++ app/src/main/java/org/sinisterstuf/guesstheanimal/ui/GreetingActivity.java
@@ -9,25 +9,30 @@ import org.sinisterstuf.guesstheanimal.Animal;
 import org.sinisterstuf.guesstheanimal.Game;
 import org.sinisterstuf.guesstheanimal.R;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class GreetingActivity extends Activity {
-	
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.greeting);
+        ButterKnife.bind(this);
     }
 
-	public void guessAnAnimal(View view) {
-		Animal firstAnimal = new Animal("a pig", "Does it have a curly tail?",  true);
-		firstAnimal.prevAnimal = null;
-		firstAnimal.noAnimal = new Animal("a dog", "Does it have 4 legs?", true);
-		// reverse-linked list traversal:
-		firstAnimal.noAnimal.prevAnimal = firstAnimal;
-		Game.firstAnimal = firstAnimal;
-		Intent intent = new Intent(this, GuessActivity.class);
-		intent.putExtra(Animal.ANIMAL, firstAnimal);
-		intent.putExtra(GuessActivity.FINAL_GUESS, false);
-		startActivity(intent);
-	}
+    @OnClick(R.id.okButton)
+    public void guessAnAnimal(View view) {
+        Animal firstAnimal = new Animal("a pig", "Does it have a curly tail?", true);
+        firstAnimal.prevAnimal = null;
+        firstAnimal.noAnimal = new Animal("a dog", "Does it have 4 legs?", true);
+        // reverse-linked list traversal:
+        firstAnimal.noAnimal.prevAnimal = firstAnimal;
+        Game.firstAnimal = firstAnimal;
+        Intent intent = new Intent(this, GuessActivity.class);
+        intent.putExtra(Animal.ANIMAL, firstAnimal);
+        intent.putExtra(GuessActivity.FINAL_GUESS, false);
+        startActivity(intent);
+    }
 
 }
--- app/src/main/java/org/sinisterstuf/guesstheanimal/ui/LearnNameActivity.java
+++ app/src/main/java/org/sinisterstuf/guesstheanimal/ui/LearnNameActivity.java
@@ -9,28 +9,36 @@ import android.widget.EditText;
 import org.sinisterstuf.guesstheanimal.Animal;
 import org.sinisterstuf.guesstheanimal.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class LearnNameActivity extends Activity {
-	
-	private Animal previous;
-	private boolean prevReq;
+
+    @BindView(R.id.userInput)
+    EditText userInput;
+
+    private Animal previous;
+    private boolean prevReq;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.learn_name);
+        ButterKnife.bind(this);
         Intent intent = getIntent();
-        previous = (Animal)intent.getSerializableExtra(Animal.ANIMAL);
+        previous = (Animal) intent.getSerializableExtra(Animal.ANIMAL);
         prevReq = intent.getBooleanExtra(Animal.NEXT_REQ, false);
     }
-    
-    public void learn(View view) {
-    	EditText textInput = (EditText)findViewById(R.id.userInput);
-    	String name = textInput.getText().toString();
-    	Intent intent = new Intent(this, LearnQuestionActivity.class);
-    	intent.putExtra(Animal.NAME, name);
-    	intent.putExtra(Animal.ANIMAL, previous);
-    	intent.putExtra(Animal.NEXT_REQ, prevReq);
-    	startActivity(intent);
+
+    @OnClick(R.id.okButton)
+    public void learn() {
+        String name = userInput.getText().toString();
+        Intent intent = new Intent(this, LearnQuestionActivity.class);
+        intent.putExtra(Animal.NAME, name);
+        intent.putExtra(Animal.ANIMAL, previous);
+        intent.putExtra(Animal.NEXT_REQ, prevReq);
+        startActivity(intent);
     }
-    
+
 }
